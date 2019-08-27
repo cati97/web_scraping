a976e84c7917d9486e5f7cc289f24414156cf14f
Katarzyna Siwiec <katasiw97@gmail.com>
2019-08-27 21:28:56 +0200

    added page_count using regular expressions

--- books_scraping/pages/books.py
+++ books_scraping/pages/books.py
@@ -1,3 +1,4 @@
+import re
 from bs4 import BeautifulSoup
 from books_scraping.locators.book_locator import BookLocator
 from books_scraping.parsers.book_parser import BookParser
@@ -12,3 +13,10 @@ class BookPage:
         locator = BookLocator.BOOKS
         books = self.soup.select(locator)
         return [BookParser(e) for e in books]
+
+    @property
+    def page_count(self):
+        content = self.soup.select_one(BookLocator.PAGER).string
+        pattern = 'Page \d* of (\d*)'
+        matches = re.search(pattern, content)
+        return int(matches.group(1))
