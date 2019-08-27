4e43af1227b2cf5261cec7eb0fa4371c75576809
Katarzyna Siwiec <katasiw97@gmail.com>
2019-08-27 21:29:28 +0200

    changed hard coded 50 pages into extracting data

--- books_scraping/app.py
+++ books_scraping/app.py
@@ -6,7 +6,7 @@ page_content = requests.get('http://books.toscrape.com/').content
 page = BookPage(page_content)
 books = page.books
 
-for page in range(1, 50):
+for page in range(1, page.page_count):
     url = f'http://books.toscrape.com/catalogue/page-{page+1}.html'
     page_content = requests.get(url).content
     page = BookPage(page_content)
