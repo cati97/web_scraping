541ee02c4bf32d6520a3a8446be3b1b5805f7372
Katarzyna Siwiec <katasiw97@gmail.com>
2019-08-28 12:46:16 +0200

    added logger that inherits from the app logger

--- books_scraping/pages/books.py
+++ books_scraping/pages/books.py
@@ -1,7 +1,10 @@
 import re
+import logging
 from bs4 import BeautifulSoup
 from books_scraping.locators.book_locator import BookLocator
 from books_scraping.parsers.book_parser import BookParser
+
+logger = logging.getLogger('book_scraping.books')  # inherits basicConfig from parent
 
 
 class BookPage:
@@ -12,6 +15,7 @@ class BookPage:
     def books(self):
         locator = BookLocator.BOOKS
         books = self.soup.select(locator)
+        logging.debug('Creating book objects.....')
         return [BookParser(e) for e in books]
 
     @property
