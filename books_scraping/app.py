c756dfe853d6a481034d0bd3b9e2b7832d76932a
Katarzyna Siwiec <katasiw97@gmail.com>
2019-08-28 12:45:19 +0200

    added logger

--- books_scraping/app.py
+++ books_scraping/app.py
@@ -1,5 +1,14 @@
 import requests
+import logging
 from books_scraping.pages.books import BookPage
+
+logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s: %(lineno)d] %(message)s',
+                    datefmt='%d-%m-%Y %H:%M:%S',
+                    level=logging.DEBUG,
+                    filename='logs.txt')
+
+logger = logging.getLogger('books_scraping')
+
 
 page_content = requests.get('http://books.toscrape.com/').content
 
@@ -7,8 +16,9 @@ page = BookPage(page_content)
 books = page.books
 
 for page in range(1, page.page_count):
-    url = f'http://books.toscrape.com/catalogue/page-{page+1}.html'
+    url = f'http://books.toscrape.com/catalogue/page-{page + 1}.html'
     page_content = requests.get(url).content
+    logger.debug(f'Managed to get page_content from page nr `{page + 1}`')
     page = BookPage(page_content)
     books.extend(page.books)
 
